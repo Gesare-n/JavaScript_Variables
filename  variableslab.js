fceec11c9bd60293c84d00dd70d7351456342906
Gesare-n <114504698+Gesare-n@users.noreply.github.com>
2023-12-18 08:50:41 +0300

    Add files via upload

---
+++  variableslab.js
@@ -0,0 +1,22 @@
+// index.js
+
+// Task 1: Declare and Initialize companyName
+let companyName = 'Scubber';
+
+// Task 2: Use const for companyName
+const companyNameConst = 'Scubber';
+
+// Task 3: Declare and Initialize mostProfitableNeighborhood
+let mostProfitableNeighborhood = 'Buffaloclose';
+
+// Task 4: Declare and Modify companyCeo
+let companyCeo = 'Dorothy';
+companyCeo = 'Maria';
+
+// export variables for testing
+module.exports = {
+ companyName,
+ companyNameConst,
+ mostProfitableNeighborhood,
+ companyCeo,
+};
